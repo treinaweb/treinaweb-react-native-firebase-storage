8226f70d5771c20dad5c97aae7c28e447cc0b2a1
hanashiro <hanashiro@Felipes-MacBook-Pro.local>
2019-06-11 08:59:38 -0300

    aula-04-03

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, SafeAreaView} from 'react-native';
+import {Platform, StyleSheet, Text, SafeAreaView, ScrollView, Button} from 'react-native';
 
 import DirectoryList from './app/components/DirectoryList';
 import Breadcrumb from './app/components/Breadcrumb';
+import ImageList from './app/components/ImageList';
 
 export default class App extends Component {
 
@@ -19,6 +20,24 @@ export default class App extends Component {
         }
       }
     },
+    imageList: [
+      {uri: 'https://hips.hearstapps.com/clv.h-cdn.co/assets/cm/15/10/1024x1321/54f0f1d749d57_-_clx0704cover_1.jpg'},
+      {uri: 'https://hips.hearstapps.com/clv.h-cdn.co/assets/cm/15/10/1024x1321/54f0f1d749d57_-_clx0704cover_1.jpg'},
+      {uri: 'https://hips.hearstapps.com/clv.h-cdn.co/assets/cm/15/10/1024x1321/54f0f1d749d57_-_clx0704cover_1.jpg'},
+      {uri: 'https://hips.hearstapps.com/clv.h-cdn.co/assets/cm/15/10/1024x1321/54f0f1d749d57_-_clx0704cover_1.jpg'},
+      {uri: 'https://hips.hearstapps.com/clv.h-cdn.co/assets/cm/15/10/1024x1321/54f0f1d749d57_-_clx0704cover_1.jpg'},
+      {uri: 'https://hips.hearstapps.com/clv.h-cdn.co/assets/cm/15/10/1024x1321/54f0f1d749d57_-_clx0704cover_1.jpg'},
+      {uri: 'https://hips.hearstapps.com/clv.h-cdn.co/assets/cm/15/10/1024x1321/54f0f1d749d57_-_clx0704cover_1.jpg'},
+      {uri: 'https://hips.hearstapps.com/clv.h-cdn.co/assets/cm/15/10/1024x1321/54f0f1d749d57_-_clx0704cover_1.jpg'},
+      {uri: 'https://hips.hearstapps.com/clv.h-cdn.co/assets/cm/15/10/1024x1321/54f0f1d749d57_-_clx0704cover_1.jpg'},
+      {uri: 'https://hips.hearstapps.com/clv.h-cdn.co/assets/cm/15/10/1024x1321/54f0f1d749d57_-_clx0704cover_1.jpg'},
+      {uri: 'https://hips.hearstapps.com/clv.h-cdn.co/assets/cm/15/10/1024x1321/54f0f1d749d57_-_clx0704cover_1.jpg'},
+      {uri: 'https://hips.hearstapps.com/clv.h-cdn.co/assets/cm/15/10/1024x1321/54f0f1d749d57_-_clx0704cover_1.jpg'},
+      {uri: 'https://hips.hearstapps.com/clv.h-cdn.co/assets/cm/15/10/1024x1321/54f0f1d749d57_-_clx0704cover_1.jpg'},
+      {uri: 'https://hips.hearstapps.com/clv.h-cdn.co/assets/cm/15/10/1024x1321/54f0f1d749d57_-_clx0704cover_1.jpg'},
+      {uri: 'https://hips.hearstapps.com/clv.h-cdn.co/assets/cm/15/10/1024x1321/54f0f1d749d57_-_clx0704cover_1.jpg'},
+      {uri: 'https://hips.hearstapps.com/clv.h-cdn.co/assets/cm/15/10/1024x1321/54f0f1d749d57_-_clx0704cover_1.jpg'},
+    ],
     directoryList: [
       {name: 'pasta 1'},
       {name: 'pasta 2'},
@@ -32,7 +51,14 @@ export default class App extends Component {
     return (
       <SafeAreaView style={styles.container}>
         <Breadcrumb directory={state.currentDirectory} />
-        <DirectoryList directories={state.directoryList} />
+
+        <ScrollView style={styles.scrollView} >
+          <DirectoryList directories={state.directoryList} />
+          <ImageList images={state.imageList} />
+        </ScrollView>
+
+        <Button title="Add" />
+        
       </SafeAreaView>
     );
   }
@@ -42,5 +68,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F5FCFF',
+  },
+  scrollView: {
+    flex: 1,
+    marginTop: 10
   }
 });
---
+++ app/components/ImageList.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import {StyleSheet, View, Image, Dimensions, TouchableHighlight} from 'react-native';
+
+export default class ImageList extends Component{
+    static defaultProps = {
+        images: [],
+        onSelect: () => {}
+    }
+
+    render(){
+        const {props} = this;
+        return (
+            <View style={styles.imageContainer} >
+                {props.images.map((image, index) => {
+                    return (
+                        <TouchableHighlight key={index} onPress={() => props.onPress(image)}>
+                            <Image style={styles.image} source={{uri: image.uri}} />
+                        </TouchableHighlight>
+                    )
+                })}
+            </View>
+        );
+    }
+}
+
+const {width} = Dimensions.get('screen'),
+    size = (width / 3) - 8;
+
+const styles = StyleSheet.create({
+    imageContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        flexWrap: 'wrap'
+    },
+    image: {
+        width: size,
+        height: size,
+        margin: 4
+    }
+})
